f8f56772141813f32c127b167bde7dc9ad267663
Shimamura Yuta <shimamura@agora-c.com>
2024-04-12 13:43:59 +0900

    20240412 commit

--- lib/screens/plan_timeline.dart
+++ lib/screens/plan_timeline.dart
@@ -2,8 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:miel_work_app/common/functions.dart';
 import 'package:miel_work_app/common/style.dart';
-import 'package:miel_work_app/models/organization_group.dart';
-import 'package:miel_work_app/models/plan.dart';
 import 'package:miel_work_app/providers/home.dart';
 import 'package:miel_work_app/providers/login.dart';
 import 'package:miel_work_app/screens/plan_add.dart';
@@ -11,7 +9,6 @@ import 'package:miel_work_app/screens/plan_mod.dart';
 import 'package:miel_work_app/services/config.dart';
 import 'package:miel_work_app/services/plan.dart';
 import 'package:miel_work_app/widgets/custom_calendar_timeline.dart';
-import 'package:miel_work_app/widgets/form_label.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart' as sfc;
 
 class PlanTimelineScreen extends StatefulWidget {
@@ -49,37 +46,24 @@ class _PlanTimelineScreenState extends State<PlanTimelineScreen> {
       case sfc.CalendarElement.appointment:
       case sfc.CalendarElement.agenda:
         sfc.Appointment appointmentDetails = details.appointments![0];
-        if (widget.loginProvider.user?.admin == true) {
-          pushScreen(
-            context,
-            PlanModScreen(
-              loginProvider: widget.loginProvider,
-              homeProvider: widget.homeProvider,
-              planId: '${appointmentDetails.id}',
-            ),
-          );
-        } else {
-          showDialog(
-            context: context,
-            builder: (context) => PlanDialog(
-              loginProvider: widget.loginProvider,
-              homeProvider: widget.homeProvider,
-              planId: '${appointmentDetails.id}',
-            ),
-          );
-        }
+        pushScreen(
+          context,
+          PlanModScreen(
+            loginProvider: widget.loginProvider,
+            homeProvider: widget.homeProvider,
+            planId: '${appointmentDetails.id}',
+          ),
+        );
         break;
       case sfc.CalendarElement.calendarCell:
-        if (widget.loginProvider.user?.admin == true) {
-          pushScreen(
-            context,
-            PlanAddScreen(
-              loginProvider: widget.loginProvider,
-              homeProvider: widget.homeProvider,
-              date: details.date ?? DateTime.now(),
-            ),
-          );
-        }
+        pushScreen(
+          context,
+          PlanAddScreen(
+            loginProvider: widget.loginProvider,
+            homeProvider: widget.homeProvider,
+            date: details.date ?? DateTime.now(),
+          ),
+        );
         break;
       default:
         break;
@@ -144,112 +128,5 @@ class _PlanTimelineScreenState extends State<PlanTimelineScreen> {
 class _DataSource extends sfc.CalendarDataSource {
   _DataSource(List<sfc.Appointment> source) {
     appointments = source;
-  }
-}
-
-class PlanDialog extends StatefulWidget {
-  final LoginProvider loginProvider;
-  final HomeProvider homeProvider;
-  final String planId;
-
-  const PlanDialog({
-    required this.loginProvider,
-    required this.homeProvider,
-    required this.planId,
-    super.key,
-  });
-
-  @override
-  State<PlanDialog> createState() => _PlanDialogState();
-}
-
-class _PlanDialogState extends State<PlanDialog> {
-  PlanService planService = PlanService();
-  String titleText = '';
-  String groupText = '';
-  String dateTimeText = '';
-  Color color = Colors.transparent;
-  String memoText = '';
-
-  void _init() async {
-    PlanModel? plan = await planService.selectData(id: widget.planId);
-    if (plan == null) {
-      if (!mounted) return;
-      showMessage(context, '予定データの取得に失敗しました', false);
-      Navigator.pop(context);
-      return;
-    }
-    titleText = '[${plan.category}]${plan.subject}';
-    for (OrganizationGroupModel group in widget.homeProvider.groups) {
-      if (group.id == plan.groupId) {
-        groupText = group.name;
-      }
-    }
-    dateTimeText =
-        '${dateText('yyyy/MM/dd HH:mm', plan.startedAt)}〜${dateText('yyyy/MM/dd HH:mm', plan.endedAt)}';
-    color = plan.categoryColor;
-    memoText = plan.memo;
-    setState(() {});
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _init();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return AlertDialog(
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(8)),
-      ),
-      content: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          ListTile(
-            tileColor: color,
-            title: Text(
-              titleText,
-              style: const TextStyle(color: kWhiteColor),
-            ),
-          ),
-          const SizedBox(height: 8),
-          FormLabel(
-            label: '公開グループ',
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 4),
-              child: Text(
-                groupText,
-                style: const TextStyle(fontSize: 16),
-              ),
-            ),
-          ),
-          const SizedBox(height: 8),
-          FormLabel(
-            label: '予定期間',
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 4),
-              child: Text(
-                dateTimeText,
-                style: const TextStyle(fontSize: 16),
-              ),
-            ),
-          ),
-          const SizedBox(height: 8),
-          FormLabel(
-            label: 'メモ',
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 4),
-              child: Text(
-                memoText,
-                style: const TextStyle(fontSize: 16),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
   }
 }
