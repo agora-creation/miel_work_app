4ea49f28627d264e9b78c2c835a7272704933b48
Shimamura Yuta <shimamura@agora-c.com>
2024-08-01 12:55:13 +0900

    20240801 commit

---
+++ lib/screens/plan_detail.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PlanDetailScreen extends StatefulWidget {
+  const PlanDetailScreen({super.key});
+
+  @override
+  State<PlanDetailScreen> createState() => _PlanDetailScreenState();
+}
+
+class _PlanDetailScreenState extends State<PlanDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
